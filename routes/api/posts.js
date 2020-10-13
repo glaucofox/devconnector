191d491be6b3d63ea19dbf4f4483783de5c9c56b
Glauco Fox <glauco.fox@gmail.com>
2020-10-12 21:15:48 -0300

    Added get post by ID endpoint

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -61,5 +61,25 @@ router.get('/', async (req, res) => {
     }
 });
 
+// @route   GET api/posts/:id
+// @desc    Get post by post ID
+// @access  Private
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
